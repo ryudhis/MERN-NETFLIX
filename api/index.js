d2879f5af1bfbf5f917f69adfbe4391f3b97657c
Ryudhis <ryudhis.id@gmail.com>
2025-09-15 19:07:06 +0700

    feat: test deploy

--- api/index.js
+++ api/index.js
@@ -2,34 +2,28 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const authRoute = require("#routes/auth");
-const usersRoute = require("#routes/users");
-const moviesRoute = require("#routes/movies");
-const listsRoute = require("#routes/lists");
+
+const authRoute = require("./routes/auth");
+const usersRoute = require("./routes/users");
+const moviesRoute = require("./routes/movies");
+const listsRoute = require("./routes/lists");
 
 dotenv.config();
+
 mongoose
   .connect(process.env.MONGO_URL)
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.error("MongoDB connection error:", err);
-  });
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 app.use(express.json());
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/movies", moviesRoute);
 app.use("/api/lists", listsRoute);
+
 app.get("/", (req, res) => {
   res.send("Hello from Express on Vercel!");
 });
 
-app.listen(8800, () => {
-  console.log("API is running on http://localhost:8800");
-});
-
 module.exports = app;
-
-
