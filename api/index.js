dd1bde63528f3364c5643001f987384af67bda44
Ryudhis <ryudhis.id@gmail.com>
2025-09-15 20:07:19 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,6 +4,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const routes = require("#routes/index");
 const { createServer } = require("http");
+const User = require("#models/User");
+const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
 dotenv.config();
 
@@ -18,6 +21,30 @@ routes(app);
 
 app.get("/", (req, res) => {
   res.send("Hello from Express on Vercel!");
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(401).json("Wrong credentials!");
+    const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+    const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+    originalPassword !== req.body.password &&
+      res.status(401).json("Wrong credentials!");
+
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.SECRET_KEY,
+      { expiresIn: "1h" }
+    );
+    const { password, ...info } = user._doc;
+    res.status(200).json({ ...info, accessToken });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 server.listen(8800, () => {
