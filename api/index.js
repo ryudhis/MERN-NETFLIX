ad92e54379bde69dfa9e0c474408b4e625b0d09a
Ryudhis <ryudhis.id@gmail.com>
2025-09-15 18:57:57 +0700

    feat: test deploy

--- api/index.js
+++ api/index.js
@@ -6,7 +6,6 @@ const authRoute = require("#routes/auth");
 const usersRoute = require("#routes/users");
 const moviesRoute = require("#routes/movies");
 const listsRoute = require("#routes/lists");
-const serverless = require("serverless-http");
 
 dotenv.config();
 mongoose
@@ -30,3 +29,7 @@ app.get("/", (req, res) => {
 app.listen(8800, () => {
   console.log("API is running on http://localhost:8800");
 });
+
+module.exports = app;
+
+
